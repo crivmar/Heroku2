5bc103c12ce2a4ea2dd0e1a19184ec5ea7a70307
Carlos Rivero <carlosrivero1988@gmail.com>
2021-05-06 11:38:43 +0200

    Añadido arhcivo base con el manejo del documento json

---
+++ app.py
@@ -0,0 +1,21 @@
+## IMPORTAR MÓDULOS ##
+
+from flask import Flask, render_template, request, abort
+import os, json
+
+app= Flask (__name__)
+
+## ABRIR ARCHIVO Y GUARDAR DATOS ##
+
+with open ('./MSX.json') as f:
+    documento=json.load(f)
+
+## PROGRAMA PRINCIPAL ##
+
+
+
+
+
+
+port=os.environ["PORT"]
+app.run('0.0.0.0', int(port), debug=False)
